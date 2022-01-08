bdbe61c399e65fa0af1e22a3f759b085b579d804
yeonfish6040 <yeonfish6040@gmail.com>
2022-01-08 20:10:50 +0900

    log

--- pwCracker/index.py
+++ pwCracker/index.py
@@ -1,7 +1,13 @@
 from PyQt5 import QtCore
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QToolTip, QProgressBar, QLabel, QLineEdit
 from PyQt5.QtGui import QFont
 from PyQt5.QtCore import QBasicTimer
+from PyQt5.QtWidgets import * 
+from PyQt5 import uic 
+from PyQt5.QtCore import *
+from multiprocessing import Process, Queue
+from multiprocessing import Pool
+from pyee import EventEmitter
+import multiprocessing as mp
 
 import sys
 import paramiko
@@ -9,7 +15,8 @@ import os
 import re
 import time
 import functions
-import threading
+
+ee = EventEmitter()
 
 cli = paramiko.SSHClient()
 cli.set_missing_host_key_policy(paramiko.AutoAddPolicy)
@@ -29,10 +36,15 @@ class MyApp(QWidget):
         self.lblUser.setAlignment(QtCore.Qt.AlignLeft)
         self.lblUser.move(0, 43)
 
-        self.lbl = QLabel(self)
-        self.lbl.move(0, 100)
-        self.lbl.setText("대기중")
-        self.lbl.repaint()
+        self.lbl1 = QLabel(self)
+        self.lbl1.move(0, 100)
+        self.lbl1.setText("waiting")
+        self.lbl1.repaint()
+
+        self.textbrowser = QTextBrowser(self)
+        self.textbrowser.setOpenExternalLinks(True)
+        self.textbrowser.move(0, 160)
+        self.textbrowser.append("waiting...")
 
         self.inputServer = QLineEdit(self)
         self.inputServer.move(150, 0)
@@ -44,28 +56,31 @@ class MyApp(QWidget):
         self.buttonStart = QPushButton(self)
         self.buttonStart.setText('Start')
         self.buttonStart.clicked.connect(self.onStart)
-        self.buttonStart.move(100, 200)
+        self.buttonStart.move(300, 200)
 
         self.buttonRestart = QPushButton(self)
         self.buttonRestart.setText('Restart')
         self.buttonRestart.clicked.connect(self.onRestart)
-        self.buttonRestart.move(200, 200)
+        self.buttonRestart.move(300, 250)
 
-        self.setWindowTitle('Password Cracker')
+        self.setWindowTitle('Password Finder')
         self.setGeometry(1000, 1000, 500, 400)
         self.show()
 
     def onChanged(self):
-        self.lbl.setText("Taget --------\nServer: %s\nUser: %s" % (self.inputServer.text(), self.inputUser.text()))
-        self.lbl.adjustSize()
+        QApplication.processEvents()
+        self.lbl1.setText("Taget --------\nServer: %s\nUser: %s" % (self.inputServer.text(), self.inputUser.text()))
+        self.lbl1.adjustSize()
     def onRestart(self):
+        QApplication.processEvents()
         os.execl(sys.executable, '"{}"'.format(sys.executable), *sys.argv)
 
-
     def onStart(self):
+        global pwMatched
         pwMatched = True
-        pwMatch(self, "a", 1, self.lbl, self.inputServer, self.inputUser)
-def pwMatch(self, start, jump, label, serverInput, userInput):
+        pwMatch("a", 1, self.textbrowser, self.inputServer, self.inputUser, time.time())
+
+def pwMatch(start, jump, textbrowser, serverInput, userInput, start_time):
     server = serverInput.text()
     user = userInput.text()
     serverInput.setDisabled(True)
@@ -75,6 +90,7 @@ def pwMatch(self, start, jump, label, serverInput, userInput):
     start_time = time.time()
     global pwMatched
     while pwMatched:
+        textbrowser.append(result)
         ok = True
         QApplication.processEvents()
         try:
@@ -90,12 +106,7 @@ def pwMatch(self, start, jump, label, serverInput, userInput):
             result = increased
         else:
             result = "a"+"a"*result.__len__()
-        label.setText(result)
-        label.adjustSize()
-        label.repaint()
-    return result, 
-    label.setText("finished!"+" - "+result+"\n"+"--- "+str(round(time.time() - start_time, 2))+" seconds --- ")
-    label.adjustSize()
+    textbrowser.append("finished!"+" - "+result+"\n"+"--- "+str(round(time.time() - start_time, 2))+" seconds --- ")
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
