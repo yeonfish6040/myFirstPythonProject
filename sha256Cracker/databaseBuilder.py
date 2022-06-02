fbf24c6d9018a0dd7d9ec3628f69e10d3cbc5a8b
yeonfish6040 <yeonfish6040@gmail.com>
2022-06-02 16:50:53 +0900

    db building..

--- sha256Cracker/databaseBuilder.py
+++ sha256Cracker/databaseBuilder.py
@@ -5,10 +5,38 @@ from PyQt5.QtWidgets import *
 from PyQt5 import uic 
 from PyQt5.QtCore import *
 
-import functions
-import sqlite3
-import os
 import sys
+import functions
+import os
+import psutil
+
+class Worker(QObject):
+    def settingData(self, data):
+        db_path = './sha256Values.db'
+        with open(db_path, 'r') as f:
+            # check file emtpy
+            if f.read() == '':
+                with open(db_path, 'w') as fs:
+                    fs.write('a§'+functions.getHash("a")+"\n")
+                string = "a"
+            else:
+                # get last string
+                with open(db_path, 'r') as f:
+                    string = f.readlines()[-1].split('§')[0]
+        stop = False
+        while not stop:
+            QApplication.processEvents()
+            increased = functions.str_increaser(string, functions.charList(), 1)
+            if increased != "str_increaser(): Input already max in charset":
+                string = increased
+            else:
+                string = "a"+"a"*string.__len__()
+            with open(db_path, 'a') as f:
+                f.write(string+"§"+functions.getHash(string)+"\n")
+            with open(db_path, 'r') as f:
+                    count = f.readlines().__len__()
+            data.lblcount.setText(string+"\n"+str(count))
+            data.lblhash.setText(functions.getHash(string))
 
 class MyApp(QWidget):
     def __init__(self):
@@ -16,39 +44,50 @@ class MyApp(QWidget):
         self.initUI()
 
     def initUI(self):
-        self.lblnotice = QLabel(self)
-        self.lblnotice.setText("char to start")
-        self.lblnotice.setAlignment(QtCore.Qt.AlignLeft)
-        self.lblnotice.move(0, 3)
-
         self.lblcount = QLabel(self)
         self.lblcount.setText("waiting...")
         self.lblcount.setAlignment(QtCore.Qt.AlignLeft)
-        self.lblcount.move(100, 20)
+        self.lblcount.move(30, 10)
         self.lblcount.resize(150, 30)
 
         self.lblhash = QLabel(self)
         self.lblhash.setText("waiting...")
         self.lblhash.setAlignment(QtCore.Qt.AlignLeft)
-        self.lblhash.move(100, 60)
-        self.lblhash.resize(150, 40)
-
-        self.input = QLineEdit(self)
-        self.input.move(150, 0)
+        self.lblhash.move(30, 50)
+        self.lblhash.resize(500, 40)
 
         self.buttonStart = QPushButton(self)
         self.buttonStart.setText('Start')
         self.buttonStart.clicked.connect(self.onStart)
         self.buttonStart.move(350, 0)
 
+        self.buttonStop = QPushButton(self)
+        self.buttonStop.setText('Stop')
+        self.buttonStop.clicked.connect(self.stop)
+        self.buttonStop.move(350, 20)
+        self.buttonStop.setDisabled(True)
+
         self.setWindowTitle('sha256 db builder')
         self.setGeometry(1000, 1000, 500, 400)
         self.show()
 
     def onStart(self):
-        db_path = './sha256Values.db'
-        conn = sqlite3.connect(db_path)
-        print(conn)
+        self.buttonStart.setDisabled(True)
+        self.buttonStop.setDisabled(False)
+        self.thread = QThread()
+        self.worker = Worker()
+        self.worker.moveToThread(self.thread)
+        self.thread.setDaemon = True
+        self.thread.started.connect(lambda: self.worker.settingData(self))
+        self.thread.start()
+
+    def stop(self):
+        self.buttonStop.setDisabled(True)
+        self.buttonStart.setDisabled(False)
+        QApplication.processEvents()
+        p = psutil.Process(os.getpid())
+        p.terminate()
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
--- sha256Cracker/functions.py
+++ sha256Cracker/functions.py
@@ -21,3 +21,10 @@ def str_increaser(mystr, charset, n_increase):
             mystr = local_increase(mystr, charset)
 
     return mystr
+
+def charList():
+    return ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '`', '~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '{', '[', '}', ']', '|', '\\', ':', ';', '"', '\'', ',', '<', '.', '>', '/', '?', ' ']
+
+def getHash(string):
+    import hashlib
+    return hashlib.sha256(string.encode()).hexdigest()
